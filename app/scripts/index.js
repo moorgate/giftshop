f0d73c1e2814cc842038677a147dfcee1c8bb769
Dorokhovich Mikhail <dorokhovich.mikhail@vereign.com>
2019-03-19 00:20:34 +0300

    add jquery and modal and wrapper

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,20 @@
 import '../styles/main.scss'
+import jQuery from 'jquery'
 
 if (process.env.NODE_ENV !== 'production') {
   require('../index.pug')
 }
+
+var buttons = jQuery('button.buy-btn')
+var modal = jQuery('#modal')
+var wrapper = jQuery('#wrapper')
+var body = jQuery('body')
+buttons.click(function (e) {
+  e.preventDefault()
+  if (modal) {
+    console.log(e, modal)
+    modal.addClass('active')
+    wrapper.addClass('overlapped')
+    body.addClass('overlapped')
+  }
+})

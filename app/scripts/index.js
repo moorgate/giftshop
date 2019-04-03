7085b7a3aacfd56c8743ba7faed9f9d3779318da
Dorokhovich Mikhail <dorokhovich.mikhail@vereign.com>
2019-04-03 22:13:31 +0300

    replace some  of comments from the template to the mixins

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -16,64 +16,31 @@ function isBlank (obj) {
 jQuery(document).ready(function () {
   var order = new Order()
   var buttons = jQuery('button.buy-btn')
-  var modal = jQuery('#modal')
+  // var modal = jQuery('#modal')
   var paymentModal = jQuery('#payment-modal')
   var wrapper = jQuery('#wrapper')
   var body = jQuery('body')
   var callbackButton = jQuery('.callback-btn')
   var callbackModal = jQuery('#callback-modal')
-  var promoForm = jQuery('#promo')
-  var promoInput = promoForm.find('input[type="text"]')
-  var promoButton = promoForm.find('input[type="submit"]')
-  var promoOK = promoForm.find('.ok')
-  var promoErr = promoForm.find('.err')
-  var sizesButtons = jQuery('#item-sizes')
-  var sizes = sizesButtons.find('.size')
-
-  function disablePromoCode () {
-    promoInput.val(order.promoCode)
-    promoInput.attr('disabled', true)
-    promoButton.attr('disabled', true)
-  }
+  // var buttonProceedPayment = modal.find('button.proceed-payment')
 
   /**
-   * для выбора размеров
+   * При закрытии модального окна необходимо сбросить выбранный размер
    */
-  sizes.click(function (e) {
-    var target = jQuery(e.target)
-    order.selectedSize = target.attr('value')
-    target.addClass('active')
-    sizes.each(function (index) {
-      var size = jQuery(this)
-      if (size.attr('value') !== order.selectedSize) {
-        size.removeClass('active')
+  function closeModals (modal) {
+    return function (e) {
+      if (e) e.preventDefault()
+      if (wrapper && modal && body && wrapper.hasClass('overlapped')) {
+        modal.removeClass('active')
+        wrapper.removeClass('overlapped')
+        body.removeClass('overlapped')
+        paymentModal.removeClass('active')
+        callbackModal.removeClass('active')
+
+        order.selectedSize = null
       }
-    })
-  })
-
-  if (order.promoCode) {
-    disablePromoCode()
-  }
-
-  promoButton.click(function (e) {
-    e.preventDefault()
-    order.promoCode = promoInput.val()
-    if (!isBlank(order.promoCode)) {
-      jQuery.post(promoCodeUrl, {promoCode: order.promoCode})
-        .done(function (data) {
-          promoOK.addClass('active')
-          promoErr.removeClass('active')
-          window.localStorage.setItem('promoCode', order.promoCode)
-          disablePromoCode()
-        })
-        // eslint-disable-next-line handle-callback-err
-        .catch(err => {
-          promoOK.removeClass('active')
-          promoErr.addClass('active')
-          window.localStorage.removeItem('promoCode')
-        })
     }
-  })
+  }
 
   if (callbackButton) {
     callbackButton.click(function (e) {
@@ -86,7 +53,7 @@ jQuery(document).ready(function () {
           wrapper.addClass('overlapped')
         } else {
           paymentModal.removeClass('active')
-          modal.removeClass('active')
+          body.find("[id^='modal']").removeClass('active')
         }
 
         callbackModal.addClass('active')
@@ -94,37 +61,22 @@ jQuery(document).ready(function () {
     })
   }
 
-  /**
-   * При закрытии модального окна необходимо сбросить выбранный размер
-   */
-  function closeModals (e) {
-    if (e) e.preventDefault()
-    if (wrapper && modal && body && wrapper.hasClass('overlapped')) {
-      modal.removeClass('active')
-      wrapper.removeClass('overlapped')
-      body.removeClass('overlapped')
-      paymentModal.removeClass('active')
-      callbackModal.removeClass('active')
-
-      order.selectedSize = null
-    }
-  }
-
   jQuery(document).keyup(function (e) {
     if (e.key === 'Escape') {
-      closeModals(e)
+      closeModals(body.find('[id^=modal]'))(e)
     }
   })
+
   if (screen.width < 512) {
     jQuery('.comments').slick({
-      infinite: true,
+      infinite: false,
       arrows: false,
       slidesToShow: 1,
       slidesToScroll: 1
     })
   } else {
     jQuery('.comments').slick({
-      infinite: true,
+      infinite: false,
       arrows: false,
       slidesToShow: 3,
       slidesToScroll: 3
@@ -133,6 +85,72 @@ jQuery(document).ready(function () {
 
   buttons.click(function (e) {
     e.preventDefault()
+
+    var modalId = jQuery(e.target).attr('set-id')
+
+    var promoForm = jQuery(`#promo-${modalId}`)
+    var promoInput = promoForm.find('input[type="text"]')
+    var promoButton = promoForm.find('input[type="submit"]')
+    var promoOK = promoForm.find('.ok')
+    var promoErr = promoForm.find('.err')
+    var sizesButtons = jQuery(`#item-sizes-${modalId}`)
+    var sizes = sizesButtons.find('.size')
+    var modal = body.find(`#modal-${modalId}`)
+    var buttonProceedPayment = modal.find('button.proceed-payment')
+
+    /**
+     * выключение формы ввода промо кода
+     */
+    function disablePromoCode () {
+      promoInput.val(order.promoCode)
+      promoInput.attr('disabled', true)
+      promoButton.attr('disabled', true)
+    }
+
+    /**
+     * Обработчик нажатия на размер
+     */
+    function onSizeClick (e) {
+      var target = jQuery(e.target)
+      order.selectedSize = target.attr('value')
+      target.addClass('active')
+      sizes.each(function (index) {
+        var size = jQuery(this)
+        if (size.attr('value') !== order.selectedSize) {
+          size.removeClass('active')
+        }
+      })
+    }
+
+    /**
+     * для выбора размеров
+     */
+    sizes.click(onSizeClick)
+
+    if (order.promoCode) {
+      disablePromoCode()
+    }
+
+    promoButton.click(function (e) {
+      e.preventDefault()
+      order.promoCode = promoInput.val()
+      if (!isBlank(order.promoCode)) {
+        jQuery.post(promoCodeUrl, {promoCode: order.promoCode})
+          .done(function (data) {
+            promoOK.addClass('active')
+            promoErr.removeClass('active')
+            window.localStorage.setItem('promoCode', order.promoCode)
+            disablePromoCode()
+          })
+          // eslint-disable-next-line handle-callback-err
+          .catch(err => {
+            promoOK.removeClass('active')
+            promoErr.addClass('active')
+            window.localStorage.removeItem('promoCode')
+          })
+      }
+    })
+
     if (
       modal &&
       wrapper &&
@@ -153,7 +171,7 @@ jQuery(document).ready(function () {
         variableWidth: true
       })
 
-      modal.find('button.proceed-payment').click(function (e) {
+      buttonProceedPayment.click(function (e) {
         e.preventDefault()
         modal.removeClass('active')
         paymentModal.addClass('active')
@@ -162,7 +180,7 @@ jQuery(document).ready(function () {
   })
 
   wrapper.click(function (e) {
-    closeModals(e)
+    closeModals(body.find('[id^=modal]'))(e)
   })
 })
 
